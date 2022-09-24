763a06fd25000ef69ccaa22ba263e484ad94dfd3
DBVTrindade <75432951+LKMTec@users.noreply.github.com>
2022-09-24 14:38:51 -0300

    .

--- gcp-main.tf
+++ gcp-main.tf
@@ -1,6 +1,6 @@
 resource "google_artifact_registry_repository" "my-repo" {
   location = "us-central1"
   repository_id = "labdevops"
-  description = "Imagens Docker"
+  description = "Imagens-Docker"
   format = "DOCKER"
 }
