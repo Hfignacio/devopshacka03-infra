1ab6895091543f996d8c961b37e1d05884b1b539
DBVTrindade <75432951+LKMTec@users.noreply.github.com>
2022-09-24 14:54:23 -0300

    Update gcp-main.tf

--- gcp-main.tf
+++ gcp-main.tf
@@ -4,3 +4,15 @@ resource "google_artifact_registry_repository" "my-repo" {
   description = "Imagens-Docker"
   format = "DOCKER"
 }
+
+resource "google_sql_database_instance" "main" {
+  name             = "main-instance"
+  database_version = "MYSQL_5_7"
+  region           = "us-central1"
+
+  settings {
+    # Second-generation instance tiers are based on the machine
+    # type. See argument reference below.
+    tier = "db-n1-standard-2"
+  }
+}
